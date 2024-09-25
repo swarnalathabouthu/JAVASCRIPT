fda4cb49cab26ab32a32f914231f9c6b220f9b6f
swarnalathabouthu <swarnalathabouthu@gmail.com>
2024-09-25 23:54:59 +0530

    Add files via upload

---
+++ 19-09-2024/variables.js
@@ -0,0 +1,77 @@
+//Variables.
+var datatypevariable = "Variables:";
+console.log(datatypevariable);
+
+//Example one
+exampleone = "Example One:" 
+console.log(exampleone);
+//automatically
+a = 10;
+b = 20;
+c = a + b;
+console.log(c);
+console.log("   ")
+
+//Let
+let x = 100;
+let y = 240;
+let z = x + y;
+console.log(z);
+console.log("   ")
+
+//var
+var p = 70;
+var q = 90;
+var r = p + q;
+console.log(r);
+console.log("   ")
+
+//const
+const m = 920;
+const n = 30;
+const o = m + n;
+console.log(o);
+console.log("   ")
+
+//Example Two
+exampletwo = "Example Two:" 
+console.log(exampletwo);
+//automatically
+Iam = {
+    surname: "Bouthu",
+    name: "Swarnalatha",
+    occupation: "undergraduate student:B.Tech(CSE)",
+    age: 22
+}
+console.log(Iam);
+console.log("    ");
+
+//var
+var Father = {
+    surname: "Bouthu",
+    name: "Bapu",
+    occupation: "Agriculture",
+    age: 35
+}
+console.log(Father);
+console.log("    ");
+
+//let
+let Mother = {
+    surname: "Bouthu",
+    name: "Rajeshwari",
+    occupation: "Agriculture",
+    age: 32
+}
+console.log(Mother);
+console.log("     ");
+
+//const
+const Brother = {
+    surname: "Bouthu",
+    name: "AbhiRam",
+    occupation: "Sixth standard",
+    age: 12
+}
+console.log(Brother);
+console.log("  ");
