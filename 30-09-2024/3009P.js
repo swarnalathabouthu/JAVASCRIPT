fc24a582fa67d6d4ac578a6fc179757febe20204
swarnalathabouthu <swarnalathabouthu@gmail.com>
2024-10-14 14:06:44 +0530

    Add files via upload

---
+++ 30-09-2024/3009P.js
@@ -0,0 +1,49 @@
+//Voting age Code.
+var a = 20;
+if(a>=18){
+    console.log("Eligible")
+}else{
+    console.log("Not Eligible.")
+}
+
+//Matrial Status Code.
+var marriage = false;
+if(marriage){
+    console.log("Iam Married.")
+}else{
+    console.log("Iam Single.")
+}
+
+//Matrial Status Code.
+var happy = true;
+if(happy){
+    console.log("Iam Happy.")
+}else{
+    console.log("Iam Sad.")
+}
+
+//A day in the week code.
+var day="Thrusday"
+if (day==="Monday"){
+    console.log("yes today is monday.")
+}else if(day==="Tuesday"){
+    console.log("This is Tuesday")
+}
+else if(day==="Wednesday"){
+    console.log("This is Wednesday")
+
+}else if(day==="Thrusday"){
+    console.log("This is Thrusday")
+
+}else if(day==="Friday"){
+    console.log("This is Friday")
+
+}else if(day==="Weekend"){
+    console.log("This is Weekend")
+ }
+
+
+ //loops
+ for (var i=1;i<=5;++i){
+    console.log(i)
+ }
---
+++ 30-09-2024/loops.js
@@ -0,0 +1,62 @@
+//1.Print 10 numbers.
+//A.for Loop.
+for (var i=1;i<=10;++i){
+    console.log(i)
+ }
+//B.While Loop
+var i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+//C.Do-while Loop:
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
+
+//2.Even Numbers.
+//A.for Loop.
+for (var p = 2; p <=10; p+=2){
+    console.log(p);
+}
+//B.While Loop
+var p = 2;
+while (p <= 10) {
+    console.log(p);
+    p += 2;
+}
+//C.Do-while Loop:
+var p = 2;
+do {
+    console.log(p);
+    p += 2;
+} while (p <= 10);
+
+//3.Odd Numbers.
+//A.For Loop
+for (var i = 1; i <= 10; i += 2) {
+    console.log(i);
+}
+//B.While Loop
+var i = 1;
+while (i <= 10) {
+    console.log(i);
+    i += 2;
+}
+//C.Do while Loop
+var i = 1;
+do {
+    console.log(i);
+    i += 2;
+} while (i <= 10);
+
+//4.Grade D program
+var marks = 40; 
+if (marks >= 35 && marks <= 45) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade: Not D");
+}
+
