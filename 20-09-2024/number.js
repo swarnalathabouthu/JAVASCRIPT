3eeb91d94f38703a55851ae82923a9813fd313ba
swarnalathabouthu <swarnalathabouthu@gmail.com>
2024-10-21 15:48:29 +0530

    Add files via upload

---
+++ 20-09-2024/number.js
@@ -0,0 +1,13 @@
+//Number Methods.
+var num = 38295.98359;
+console.log(num.toString());
+console.log(num.toFixed(4));
+console.log(num.toPrecision(4));
+console.log(isNaN(num) + " " + "Swarna");
+console.log(isFinite(num));
+console.log(parseFloat(num));
+console.log(parseInt(num));
+console.log(num.toExponential());
+console.log(num.toLocaleString());
+console.log(Number(29.74));
+console.log(Number("Swarna"));
